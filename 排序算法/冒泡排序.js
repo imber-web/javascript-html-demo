8ffb6308361bedc6c5ffd273fb0d27e26b05ec8d
imber <a1157911285@163.com>
2022-06-24 12:59:15 +0800

    feat:手写排序算法-优化冒泡排序

--- 排序算法/冒泡排序.js
+++ 排序算法/冒泡排序.js
@@ -1,3 +1,4 @@
+// 基础版本  O(n^2)
 function bubbleSort(arr) {
   const len = arr.length
   for (let i = 0; i < len; i++) {
@@ -11,3 +12,17 @@ function bubbleSort(arr) {
 }
 
 console.log(bubbleSort([3, 1, 4, 2]))
+
+// 优化版本  O(n^2)
+function betterBubbleSort(arr) {
+  const len = arr.length
+  for (let i = 0; i < len; i++) {
+    // 注意差别在这行，我们对内层循环的范围作了限制
+    for (let j = 0; j < len - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        ;[arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+      }
+    }
+  }
+  return arr
+}
