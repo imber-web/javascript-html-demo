c0e0d0f18967d5c33975509985ef896def66c4d1
imber <a1157911285@163.com>
2022-06-24 17:20:30 +0800

    feat:原型链继承

---
+++ 继承/原型链继承.js
@@ -0,0 +1,31 @@
+function SuperType() {
+  this.property = true
+}
+SuperType.prototype.getSuperValue = function () {
+  return this.property
+}
+function SubType() {
+  this.subproperty = false
+}
+
+// 这里是关键，创建SuperType的实例，并将该实例赋值给SubType.prototype
+SubType.prototype = new SuperType()
+SubType.prototype.getSubValue = function () {
+  return this.subproperty
+}
+var instance = new SubType()
+console.log(instance.getSuperValue()) // true
+
+// 原型链方案存在的缺点：多个实例对引用类型的操作会被篡改。
+
+function SuperType() {
+  this.colors = ['red', 'blue', 'green']
+}
+function SubType() {}
+
+SubType.prototype = new SuperType()
+var instance1 = new SubType()
+instance1.colors.push('black')
+alert(instance1.colors) //"red,blue,green,black"
+var instance2 = new SubType()
+alert(instance2.colors) //"red,blue,green,black"
