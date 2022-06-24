cb0ed06c55dfbf2fb20531ffb15b350d72ecc233
imber <a1157911285@163.com>
2022-06-24 13:07:46 +0800

    feat:手写排序算法-优化冒泡排序O(n)

--- 排序算法/冒泡排序.js
+++ 排序算法/冒泡排序.js
@@ -26,3 +26,24 @@ function betterBubbleSort(arr) {
   }
   return arr
 }
+
+console.log(betterBubbleSort([3, 1, 4, 2]))
+
+// 优化版本 将最好情况下的时间复杂度定向优化为 O(n)
+function betterBubbleSort(arr) {
+  const len = arr.length
+  for (let i = 0; i < len; i++) {
+    // 区别在这里，我们加了一个标志位
+    let flag = false
+    for (let j = 0; j < len - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        ;[arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+        // 只要发生了一次交换，就修改标志位
+        flag = true
+      }
+    }
+    // 若一次交换也没发生，则说明数组有序，直接放过
+    if (flag == false) return arr
+  }
+  return arr
+}
