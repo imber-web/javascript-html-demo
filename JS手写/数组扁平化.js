f30414b39637a13d88b2601d35db478acfed6467
imber <a1157911285@163.com>
2022-06-25 11:29:52 +0800

    feat:迭代的方式数组扁平化

--- JS手写/数组扁平化.js
+++ JS手写/数组扁平化.js
@@ -11,3 +11,12 @@ const arr = [
   [3, [4, 5]]
 ]
 console.log(flatter(arr))
+
+function flatter(arr) {
+  if (!arr.length) return
+  while (arr.some((item) => Array.isArray(item))) {
+    arr = [].concat(...arr)
+  }
+  return arr
+}
+console.log(flatter([1, 2, [1, [2, 3, [4, 5, [6]]]]]))
